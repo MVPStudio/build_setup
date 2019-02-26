9c28655e62d01d8ccf2702e0ddfb2b4d62b66461
alyssakelley <abkelley97@gmail.com>
2019-02-26 12:07:26 -0800

    Delted test functions that require network availability, etc.

--- test_new_app_setup.py
+++ test_new_app_setup.py
@@ -11,31 +11,21 @@ base_path = os.path.abspath(os.path.dirname(__file__))
 
 path_to_test_namesapce_yml = os.path.join(base_path,
                                 'test_data/kubernetes/test_namespace1.yml')
-
 team_name = 'test1'
 
 filename = 'test1.yml'
 
 class TestNewAppSetup(unittest.TestCase):
 
-    def test_create_yml(self):
-        data_from_yml = new_app_setup.create_yml(filename, team_name)
+    def test_create_kubernetes_namespace_yml(self):
+        data_from_yml = new_app_setup.create_kubernetes_namespace_yml(filename, team_name)
 
         self.assertEqual(data_from_yml['kind'], 'Namespace')
         self.assertEqual(data_from_yml['apiVersion'], 'v1')
         self.assertEqual(data_from_yml['metadata']['name'], 'test1')
-        # self.assertEqual(data_from_yml['metadata']['labels']['name'], 'test1tag')
-
-
-    def test_create_kubernetes_namespace(self):
-        #subprocess.call(['kubectl', 'delete', team_name])
-
-        data_from_yml = new_app_setup.create_yml(filename, team_name)
-
-        namespace_name = data_from_yml['metadata']['name']
-        does_namespace_exist = subprocess.call(['kubectl', 'describe', 'namespace', namespace_name])
-
-        self.assertEqual(does_namespace_exist, 0)
+        self.assertEqual(data_from_yml['metadata']['labels']['name'],
+        	             'Hack_For_A_Cause_2019')
 
 if __name__ == '__main__':
+	# use minikube on local machine
     unittest.main()

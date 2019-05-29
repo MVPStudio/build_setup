fd0be9d35248df48e57f21ac220aeaa938c52000
alyssakelley <abkelley97@gmail.com>
2019-05-28 17:46:26 -0700

    Fixing arg parsing

--- delete_all_unused_resources.py
+++ delete_all_unused_resources.py
@@ -20,18 +20,23 @@ def main():
 
     args = parser.parse_args()
 
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    working_dir_path = os.path.join(base_path, ".working_tmp")
+
     deletion_script = "delete_unused_resources.py"
 
     list_of_unactive = find_unactive_namespaces.find_all_unactive_namespaces()
 
-    go_through_and_delete(namespaces_file, deletion_script,
-                          args.docker_u, args.docker_p, list_of_unactive)
+    go_through_and_delete(deletion_script, args.docker_u,
+                          args.docker_p, list_of_unactive,
+                          working_dir_path)
 
     print("Done deleting all of the unused resources.")
 
 
-def go_through_and_delete(namespaces_file, deletion_script,
-                          docker_u, docker_p, list_of_unactive):
+def go_through_and_delete(deletion_script, docker_u,
+                          docker_p, list_of_unactive,
+                          working_dir_path):
     """
     This function uses the unactive list of namespaces,
     and loops though those namespaces and individually calls
@@ -43,29 +48,8 @@ def go_through_and_delete(namespaces_file, deletion_script,
 
     for i in range(len(list_of_unactive)):
         team_name = list_of_unactive[i]
-
-        base_path = os.path.abspath(os.path.dirname(__file__))
-        working_dir_path = os.path.join(base_path, ".working_tmp")
-
-        namespace_path = os.path.join(working_dir_path,
-                                      (team_name + "-namespace.yml"))
-        quota_path = os.path.join(working_dir_path,
-                                  (team_name + "-quota.yml"))
-        role_path = os.path.join(working_dir_path, (team_name + "-role.yml"))
-        rolebinding_path = os.path.join(working_dir_path,
-                                        (team_name + "-rolebinding.yml"))
-        json_path = os.path.join(working_dir_path, (team_name +
-                                                    "_gcloud_service" +
-                                                    "account_key.json"))
-
-        delete_unused_resources.delete_team_files(team_name, namespace_path,
-                                                  quota_path, role_path,
-                                                  rolebinding_path)
-        delete_unused_resources.delete_kubernetes_namespace(team_name)
-        delete_unused_resources.delete_gcloud_serviceaccount(team_name,
-                                                             json_path)
-        delete_unused_resources.delete_dockerhub_repo(team_name,
-                                                      docker_u, docker_p)
+        delete_unused_resources.delete_all(team_name, docker_u,
+                                           docker_p, working_dir_path)
 
 
 if __name__ == '__main__':
